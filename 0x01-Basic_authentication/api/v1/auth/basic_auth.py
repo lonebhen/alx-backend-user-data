f838e2cf7eddd1e9ac9f688b1120fee09bac27e9
lonebhen <ben.angmortey@gmail.com>
2023-03-16 23:51:39 +0000

    auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -47,16 +47,22 @@ class BasicAuth(Auth):
             return(mail, password)
         return(None, None)
 
-    def user_object_from_credentials(
-            self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        ''' def user object from credentials '''
-        if type(user_email) != str:
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        Return a User instance based on email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
             return None
-        if type(user_pwd) != str:
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        if user_email and user_pwd:
+        try:
             users = User.search({"email": user_email})
-            for user in users:
-                if user and user.is_valid_password(user_pwd):
-                    return user
-        return None
+            if not users or users == []:
+                return None
+            for u in users:
+                if u.is_valid_password(user_pwd):
+                    return u
+            return None
+        except Exception:
+            return None
